4592b4084b916b5adb45cdc8f3baf763e5413dec
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-06-28 11:41:10 +0900

    re-added 0628

---
+++ 0628/0628/0628.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int a = 30;
+	int b = 8;
+
+	printf("a + b = %d\n", a + b);
+	printf("a - b = %d\n", a - b);
+	printf("a * b = %d\n", a * b);
+	printf("a / b = %d\n", a / b);
+	printf("a %% b = %d\n\n\n", a % b);
+
+	printf("a + b = %d\na - b = %d\na * b = %d\na / b = %d\na %% b = %d\n", a + b, a - b, a * b, a / b, a % b);
+
+	return 0;
+
+}
