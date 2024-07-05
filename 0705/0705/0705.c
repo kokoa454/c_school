45dec0dc7988dacbb896fca40e005b6fde88450e
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-07-05 14:12:07 +0900

    updated 0705

--- 0705/0705/0705.c
+++ 0705/0705/0705.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 main() {
-	int in_data;
+	/*int in_data;*/
 
-	printf("???????????:");
-	scanf("%d", &in_data);
+	int in_data1, in_data2, in_data3;
+
+	printf("数字を入力してください:");
+	scanf("%d %d %d", &in_data1, &in_data2, &in_data3);
 	
-	printf("???????%d???", in_data);
+	printf("入力した数字は%d、%d、%dです。", in_data1, in_data2, in_data3);
 }
