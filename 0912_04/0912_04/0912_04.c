adf8374ca16dd0cdcdb68fc25ae6a53336164357
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-09-12 14:21:14 +0900

    updated 0912_04

--- 0912_04/0912_04/0912_04.c
+++ 0912_04/0912_04/0912_04.c
@@ -8,15 +8,15 @@ main() {
 		printf("•Ó‚Ì’·‚³(2`9)‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢: ");
 		scanf("%d", &length);
 
-		if (length >= 2 && length <= 9) {
+		if (length >= 2 && length <= 9) { //check the entered number if true
 			for (int i = 0; i < length; i++) {
-				if (i == 0 || i == length - 1) {
+				if (i == 0 || i == length - 1) { //first and last lines in the square
 					for (int j = 0; j < length; j++) {
 						printf("* ");
 					}
 					printf("\n");
 				}
-				else {
+				else { //lines without first and last lines 
 					printf("* ");
 
 					if (length - 2 > 0) {
@@ -24,6 +24,7 @@ main() {
 							printf(" ");
 						}
 					}
+
 					printf("* ");
 					printf("\n");
 				}
