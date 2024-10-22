2c8b85722258721dadf1434b35d39886e51c4b7f
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-10-22 12:08:00 +0900

    added 1022

---
+++ 1022/1022/1022.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	char moji_d[10];
+	char* moji_pt;
+	
+	//scanf("%s", moji_d);
+	//printf("%s", moji_d);
+	
+	moji_pt = moji_d;
+	scanf("%s", moji_pt);
+	printf("%s", moji_pt);
+
+	return 0;
+}
