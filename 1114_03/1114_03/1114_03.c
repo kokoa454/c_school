8d0febf297612e7e4f4327b4402a50fb76275cb9
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-11-14 14:13:15 +0900

    updated 1114_03

--- 1114_03/1114_03/1114_03.c
+++ 1114_03/1114_03/1114_03.c
@@ -7,7 +7,20 @@ int main(void) {
 		int milli;
 	};
 
-	struct PRECIPITATION array[] = { {1, 49},{2, 60 }, {3, 115}, {4, 130 } };
+	/*
+	struct PRECIPITATION array[] = {{1, 49},{2, 60}, {3, 115}, {4, 130}};
+	*/
+
+	struct PRECIPITATION array[4];
+
+	for (int i = 0; i < 4; i++) {
+		printf("ŒŽ: ");
+		scanf("%d", &array[i].month);
+		printf("~…—Ê(ml) : ");
+		scanf("%d", &array[i].milli);
+	}
+
+	printf("\n\n");
 
 	for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++) {
 		printf("%dŒŽ %dmm\n", array[i].month, array[i].milli);
