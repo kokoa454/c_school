9831f0fd832d26ffcfa7b3292ba0051784b8013a
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-11-26 11:05:57 +0900

    updated 1126

--- 1126/1126/1126.c
+++ 1126/1126/1126.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 	char str1[7] = "DRAGON";
 	char str2[6] = "QUEST";
 	char str3[14];
+	
+	/*
 	int i, j;
 
 	for (i = 0, j = 0; str1[i]; i++, j++) {
@@ -15,7 +18,11 @@ int main(void) {
 	}
 
 	str3[j] = '\0';
+	printf("%s\n", str3);
+	*/
 
+	strcpy(str3, str1);
+	strcat(str3, str2);
 	printf("%s\n", str3);
 
 	return 0;
