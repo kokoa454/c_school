3c2c7da26cbb850a99a55c998db6858e9fc6e405
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-11-26 10:40:58 +0900

    added 1126

---
+++ 1126/1126/1126.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	char str1[7] = "DRAGON";
+	char str2[6] = "QUEST";
+	char str3[14];
+	int i, j;
+
+	for (i = 0, j = 0; str1[i]; i++, j++) {
+		str3[j] = str1[i];
+	}
+
+	for (i = 0; str2[i]; i++, j++) {
+		str3[j] = str2[i];
+	}
+
+	str3[j] = '\0';
+
+	printf("%s\n", str3);
+
+	return 0;
+}
