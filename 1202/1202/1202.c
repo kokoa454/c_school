c9c9f351cd861ad94260a97c94c32c23078e7ce3
TNOK_AKUSHIROYO <qqnq7um9k@gmail.com>
2024-12-02 13:48:09 +0900

    added 1202

---
+++ 1202/1202/1202.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	FILE* fp;
+	char str;
+	fp = fopen("Source.c", "r");
+
+	if (fp == NULL) {
+		printf("フィルオープン失敗\n");
+		return -1;
+	}
+
+	while ((str = fgetc(fp) != EOF)) {
+		printf("%c", str);
+	}
+
+	return 0;
+}
