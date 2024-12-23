c418007cb6cfcb8ecbbb1f6628649e502efbd3db
Inari Goro <qqnq7um9k@gmail.com>
2024-12-23 10:39:51 +0900

    added 1217_03

---
+++ 1217_03/1217_03/read.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "studentdata.h"
+
+void read(student *st, int *cnt_p) {
+	FILE* file_p;
+	int i = 0;
+
+	file_p = fopen("成績表.txt", "r");
+	if (file_p == NULL) {
+		printf("!ERROR: ファイルが開けません!\n\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (fscanf(file_p, "%d %s %d %c %d %d %d %d %d", &st[*cnt_p].no, &st[*cnt_p].name, &st[*cnt_p].grade, &st[*cnt_p].classNo, &st[*cnt_p].point.eng, &st[*cnt_p].point.ja, &st[*cnt_p].point.math, &st[*cnt_p].point.science, &st[*cnt_p].point.social) != EOF) {
+		(*cnt_p)++;
+	}
+
+	if (fclose(file_p) == EOF) {
+		printf("!ERROR: ファイルを閉じれません!\n\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("ファイルからの入力完了\n");
+
+	return;
+}
---
+++ 1217_03/1217_03/studentdata.h
@@ -0,0 +1,27 @@
+#ifndef STUDENTDATA_H
+#define STUDENTDATA_H
+
+typedef struct {
+	int eng;
+	int ja;
+	int math;
+	int science;
+	int social;
+}score;
+
+typedef struct {
+	int no;
+	char name[128];
+	int grade;
+	char classNo;
+	score point;
+}student;
+
+void show_header();
+void show(student*);
+void input(student*);
+void save(student*, int*);
+void read(student*, int*);
+void initialize(student*, int*);
+
+#endif
