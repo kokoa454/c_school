ca41dc6f9c7608f4d6713c54ced9c4c4a8df6e2d
Inari Goro <qqnq7um9k@gmail.com>
2025-01-09 13:36:35 +0900

    added 0109_02

---
+++ 0109_02/0109_02/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include "studentdata.h"
+
+int main(void) {
+	student school[128];
+	student* st;
+	st = school;
+	int flg = 0;
+
+	int inputNum;
+	int cnt = 0;
+	int* cnt_p;
+	cnt_p = &cnt;
+	char yOrn;
+
+	printf("****************成績票****************\n");
+
+	for (; ;) {
+		flg = 0;
+
+		printf("機能を選択してください\n");
+		printf("1: 成績表示\n");
+		printf("2: データ入力\n");
+		printf("3: ファイルに出力\n");
+		printf("4: ファイルから入力\n");
+		printf("5: 指定した学籍番号のデータを削除\n");
+		printf("6: 学年順にデータをソート\n");
+		printf("7: 学籍番号順にデータをソート\n");
+		printf("0: 終了\n");
+		printf(">>");
+		scanf("%d", &inputNum);
+
+		if (inputNum == 1) {
+			if (cnt != 0) {
+				show_header();
+				for (int i = 0; i < cnt; i++) {
+					show(&st[i]);
+				}
+				printf("\n\n");
+			}
+			else {
+				printf("!ERROR: データを1つ以上入力してください!\n\n");
+			}
+		}
+		else if (inputNum == 2) {
+			while (flg != 1) {
+				printf("%d人目の入力\n", (cnt + 1));
+				input(st + cnt);
+				cnt++;
+
+				printf("入力を終了しますか？ (y or n)\n");
+				printf(">>");
+				scanf("%c", &yOrn);
+
+				if (yOrn == 'y') {
+					flg = 1;
+				}
+			}
+		}
+		else if (inputNum == 3) {
+			if (cnt <= 0) {
+				printf("!ERROR: データを1つ以上入力してください!\n\n");
+			}
+			else {
+				save(st, cnt_p);
+			}
+		}
+		else if (inputNum == 4) {
+			initialize(st, cnt_p);
+			read(st, cnt_p);
+		}
+		else if (inputNum == 5) {
+			if (cnt <= 0) {
+				printf("!ERROR: データを1つ以上入力してください!\n\n");
+			}
+			else {
+				delete(st, cnt_p);
+			}
+		}
+		else if (inputNum == 6) {
+			sortByGrade(st, cnt_p);
+		}
+		else if (inputNum == 7) {
+			sortById(st, cnt_p);
+		}
+		else if (inputNum == 0) {
+			break;
+		}
+		else {
+			printf("!ERROR: 無効な数字です!\n");
+		}
+	}
+
+	return 0;
+}
