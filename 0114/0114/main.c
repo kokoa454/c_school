3b840db8c557a0c382127e3731e30f0dbefb3c2b
Inari Goro <qqnq7um9k@gmail.com>
2025-01-15 14:25:09 +0900

    updated 0114

--- 0114/0114/main.c
+++ 0114/0114/main.c
@@ -2,18 +2,32 @@
 
 void showCircle();
 void showCross();
+int checkIfWin();
 
 char locationData[5][5];
 
 int main(void) {	
+	int endFlg = 0;
+
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
 			locationData[i][j] = '*';
 		}
 	}
 
-	showCircle();
-	showCross();
+	while (1) {
+		showCircle();
+		if (checkIfWin() == 1) {
+			endFlg = 1;
+			break;
+		}
+
+		showCross();
+		if (checkIfWin() == 1) {
+			endFlg = 1;
+			break;
+		}
+	}
 
 	return 0;
 }
@@ -31,7 +45,7 @@ void showCircle() {
 		printf(">縦方向の座標: ");
 		scanf("%d", &inputVertical);
 
-		if (inputHorizontal > 4 || inputVertical > 4 || locationData[inputVertical][inputHorizontal] != '*') {
+		if (inputHorizontal < 0 || inputHorizontal > 4 || inputVertical < 0 || inputVertical > 4 || locationData[inputVertical][inputHorizontal] != '*') {
 			printf("不正な入力です\n");
 		}
 		else {
@@ -65,7 +79,7 @@ void showCross() {
 		printf(">縦方向の座標: ");
 		scanf("%d", &inputVertical);
 
-		if (inputHorizontal > 4 || inputVertical > 4 || locationData[inputVertical][inputHorizontal] != '*') {
+		if (inputHorizontal < 0 || inputHorizontal > 4 || inputVertical < 0 || inputVertical > 4 || locationData[inputVertical][inputHorizontal] != '*' || isdigit(inputHorizontal) != 0 || isdigit(inputVertical) != 0) {
 			printf("不正な入力です\n");
 		}
 		else {
@@ -84,4 +98,103 @@ void showCross() {
 		}
 		printf("\n");
 	}
+}
+
+int checkIfWin() {
+	int cntO = 0;
+	int cntX = 0;
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (locationData[i][j] == 'O') {
+				cntO++;
+
+				if (cntO == 5) {
+					printf("Oの勝ちです！\n");
+					return 1;
+				}
+			}
+
+			if (locationData[i][j] == 'X') {
+				cntX++;
+
+				if (cntX == 5) {
+					printf("Xの勝ちです！\n");
+					return 1;
+				}
+			}
+		}
+
+		cntO = 0;
+		cntX = 0;
+
+		for (int j = 0; j < 5; j++) {
+			if (locationData[j][i] == 'O') {
+				cntO++;
+
+				if (cntO == 5) {
+					printf("Oの勝ちです！\n");
+					return 1;
+				}
+			}
+
+			if (locationData[j][i] == 'X') {
+				cntX++;
+
+				if (cntX == 5) {
+					printf("Xの勝ちです！\n");
+					return 1;
+				}
+			}
+		}
+
+		cntO = 0;
+		cntX = 0;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		if (locationData[i][i] == 'O') {
+			cntO++;
+
+			if (cntO == 5) {
+				printf("Oの勝ちです！\n");
+				return 1;
+			}
+		}
+
+		if (locationData[i][i] == 'X') {
+			cntX++;
+
+			if (cntX == 5) {
+				printf("Xの勝ちです！\n");
+				return 1;
+			}
+		}
+
+
+		cntO = 0;
+		cntX = 0;
+	}
+
+	for (int i = 0; i < 5; i++) {
+		if (locationData[i][4 - i] == 'O') {
+			cntO++;
+
+			if (cntO == 5) {
+				printf("Oの勝ちです！\n");
+				return 1;
+			}
+		}
+
+		if (locationData[i][4 - i] == 'X') {
+			cntX++;
+
+			if (cntX == 5) {
+				printf("Xの勝ちです！\n");
+				return 1;
+			}
+		}
+	}
+
+	return 0;
 }
